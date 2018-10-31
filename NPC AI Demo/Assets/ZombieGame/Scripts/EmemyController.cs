f8fe265c4a47f4baaef31716088143a7cea97b2f
Like <chenlike695@outlook.com>
2018-10-31 11:09:39 +0800

    update enemy random move

--- NPC AI Demo/Assets/ZombieGame/Scripts/EmemyController.cs
+++ NPC AI Demo/Assets/ZombieGame/Scripts/EmemyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 /// <summary>
 /// Control the Enemy, some code is from the CharaterDemoController which not my originnal code
 /// </summary>
@@ -8,7 +9,7 @@ public class EmemyController : MonoBehaviour {
     Animator animator;
     public GameObject floorPlane;
     float rotateSpeed = 20.0f; //used to smooth out turning
-
+    float[] randommove = {0, 0};
     public Vector3 movementTargetPosition;
     public Vector3 attackPos;
     public Vector3 lookAtPos;
@@ -43,18 +44,34 @@ public class EmemyController : MonoBehaviour {
 
     }
 
-    void GetRandomPosition(){
-        Math.Random random  = new Random();
-        rando
+    float[] GetRandomPosition(){
+        System.Random random  = new System.Random();
+        float x = (float)random.NextDouble();
+        float y = (float)random.NextDouble();
+        if (random.NextDouble() > 0.5){
+            x = -x;
+        }
+        if (random.NextDouble() > 0.5){
+            y = -y;
+        }
+        float[] result = { x, y };
+        return result;
     }
 
     // Update is called once per frame
-    void Update () {
+    void FixedUpdate () {
         animator.SetInteger ("WeaponState", 0);
         GetComponent<CharacterController> ().SimpleMove (Vector3.zero);
         //transform.position = transform.position - transform.up;
         
         movementTargetPosition.y = transform.position.y;
+        if (Time.frameCount % 5 == 0){
+            randommove = GetRandomPosition();
+        }
+        Debug.Log(name + randommove[0] + " " + randommove[1]);
+        movementTargetPosition.x += randommove[0];
+        movementTargetPosition.z += randommove[1];
+ 
         Vector3 deltaTarget = movementTargetPosition - transform.position;
         lookAtPos = transform.position + deltaTarget.normalized * 2.0f;
         transform.LookAt (lookAtPos);
