c32c9e83199a71863eccbd4982d7be56a44540b1
Like <chenlike695@outlook.com>
2019-03-20 17:03:25 +0800

    remove useless asset and ignore mlagent folder

--- NPC AI Demo/Assets/AllStarCharacterLibrary/Scripts/LOD_Holder.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-public class LOD_Holder : MonoBehaviour 
-{
-	public int level;
-	public float ratio;
-	public List<SkinnedMeshRenderer> models;
-}
